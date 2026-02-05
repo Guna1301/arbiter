7719b61e3c8f14a57bf836bb8b3500af754a1a68
Guna1301 <gskotipalli2005@gmail.com>
2026-02-05 16:58:49 +0530

    redis update

--- service/app.js
+++ service/app.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 
 const redisClient = createClient({
-    url : `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`
+    url : process.env.REDIS_URL || `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
 });
 
 await redisClient.connect();
