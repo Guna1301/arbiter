64ecd79cdc71af434dc60b9a063a93a9beb4317e
Guna1301 <gskotipalli2005@gmail.com>
2026-03-05 17:18:08 +0530

    connected with arbiter web

--- nodejs-sdk/ArbiterClient.js
+++ nodejs-sdk/ArbiterClient.js
@@ -1,39 +1,65 @@
 import axios from "axios";
 import { ENDPOINTS } from "./endpoints.js";
 
-export default class ArbiterClient{
-    constructor(){
-        this.timeout = 2000;
-        this.primary = ENDPOINTS.primary;
-        this.secondary = ENDPOINTS.secondary;
+export default class ArbiterClient {
 
-        this.primaryClient = axios.create({
-            baseURL: this.primary,
-            timeout: this.timeout,
-        });
+  constructor() {
 
-        this.secondaryClient = axios.create({
-            baseURL: this.secondary,
-            timeout: this.timeout,
-        });
+    this.timeout = 2000;
+
+    this.primary = ENDPOINTS.primary;
+    this.secondary = ENDPOINTS.secondary;
+
+    this.primaryClient = axios.create({
+      baseURL: this.primary,
+      timeout: this.timeout
+    });
+
+    if (this.secondary) {
+      this.secondaryClient = axios.create({
+        baseURL: this.secondary,
+        timeout: this.timeout
+      });
     }
 
-    async decide(payload){
-        try {
-            const res = await this.primaryClient.post("/decide", payload);
-            return res.data;
-        } catch (error) {
-            if(!this.secondary){
-                throw error;
-            }
+  }
 
-            try {
-                const res = await this.secondaryClient.post("/decide", payload);
-                return res.data;
-            }catch (error) {
-                throw new Error("Both primary and secondary endpoints failed");
-            }
-            
-        }
+  async decide(payload) {
+
+    try {
+
+      const res = await this.primaryClient.post(
+        "/decide",
+        payload
+      );
+
+      return res.data;
+
+    } catch (err) {
+
+      if (!this.secondaryClient) {
+        throw err;
+      }
+
+      try {
+
+        const res = await this.secondaryClient.post(
+          "/decide",
+          payload
+        );
+
+        return res.data;
+
+      } catch (error) {
+
+        throw new Error(
+          "Arbiter decision engine unavailable (primary + secondary failed)"
+        );
+
+      }
+
     }
+
+  }
+
 }
--- nodejs-sdk/index.js
+++ nodejs-sdk/index.js
@@ -1,41 +1,163 @@
 import ArbiterClient from "./ArbiterClient.js";
+import axios from "axios";
+
+const GATEWAYS = [
+  "http://localhost:5000"
+];
 
 export function createArbiterClient(config) {
-    const client = new ArbiterClient();
-    const defaultAlgorithm = config.defaultAlgorithm || "leaky-bucket";
-    const globalWhitelist = (config.whitelist || []).map(normalizeKey);
-    const globalBlacklist = (config.blacklist || []).map(normalizeKey);
-    const globalAbuse = config.abuse || null;
 
-    return {
-        async protect({key, rule}){
-            const ruleConfig = config.rules[rule];
+  const client = new ArbiterClient();
 
-            if(!ruleConfig){
-                throw new Error(`Rule not found: ${rule}`);
-            }
+  let cloudConfig = null;
+  let version = null;
 
-            const algorithm = ruleConfig.algorithm || defaultAlgorithm;
+  async function fetchConfig(){
 
-            const policy = {
-                whitelist: (ruleConfig.policy?.whitelist || globalWhitelist).map(normalizeKey),
-                blacklist: (ruleConfig.policy?.blacklist || globalBlacklist).map(normalizeKey),
-            }
+  for(const gateway of GATEWAYS){
 
-            const abuse = ruleConfig.abuse || globalAbuse;
+    try{
 
-            return client.decide({
-                key: normalizeKey(key),
-                rule:{
-                    limit: ruleConfig.limit,
-                    window: ruleConfig.window,
-                    algorithm,
-                },
-                policy,
-                abuse
-            });
+      const res = await axios.get(
+        `${gateway}/gateway/config`,
+        {
+          headers:{
+            "x-api-key": config.apiKey
+          },
+          timeout:2000
         }
-    };
+      );
+
+      cloudConfig = res.data;
+      version = res.data.version;
+
+      return;
+
+    }catch(err){
+
+      continue;
+
+    }
+
+  }
+
+  throw new Error("All Arbiter gateways unavailable");
+
+}
+
+async function refreshConfig(){
+
+  for(const gateway of GATEWAYS){
+
+    try{
+
+      const res = await axios.get(
+        `${gateway}/gateway/config`,
+        {
+          headers:{
+            "x-api-key": config.apiKey
+          },
+          timeout:2000
+        }
+      );
+
+      if(res.data.version !== version){
+        cloudConfig = res.data;
+        version = res.data.version;
+      }
+
+      return;
+
+    }catch(err){
+      continue;
+    }
+
+  }
+}
+
+  if (config.apiKey) {
+    setInterval(refreshConfig, 60000);
+  }
+
+  return {
+
+    async init() {
+      if (config.apiKey) {
+        await fetchConfig();
+      }
+    },
+
+    async protect({ key, rule }) {
+
+    //   cloud
+
+      if (config.apiKey) {
+
+        if (!cloudConfig) {
+          await fetchConfig();
+        }
+
+        const ruleConfig =
+          cloudConfig.rules.find(r => r.name === rule);
+
+        if (!ruleConfig) {
+          throw new Error(`Rule not found: ${rule}`);
+        }
+
+        const global = cloudConfig.global;
+
+        return client.decide({
+          key: normalizeKey(key),
+          rule: {
+            limit: ruleConfig.limit,
+            window: ruleConfig.window,
+            algorithm: ruleConfig.algorithm || global.algorithm
+          },
+          policy: {
+            whitelist: ruleConfig.policy?.whitelist || global.whitelist,
+            blacklist: ruleConfig.policy?.blacklist || global.blacklist
+          },
+          abuse: ruleConfig.abuse || global.abuse
+        });
+      }
+
+    // local
+      const defaultAlgorithm = config.defaultAlgorithm || "leaky-bucket";
+
+      const globalWhitelist = (config.whitelist || []).map(normalizeKey);
+      const globalBlacklist = (config.blacklist || []).map(normalizeKey);
+      const globalAbuse = config.abuse || null;
+
+      const ruleConfig = config.rules[rule];
+
+      if (!ruleConfig) {
+        throw new Error(`Rule not found: ${rule}`);
+      }
+
+      const algorithm = ruleConfig.algorithm || defaultAlgorithm;
+
+      const policy = {
+        whitelist: (ruleConfig.policy?.whitelist || globalWhitelist).map(normalizeKey),
+        blacklist: (ruleConfig.policy?.blacklist || globalBlacklist).map(normalizeKey)
+      };
+
+      const abuse = ruleConfig.abuse || globalAbuse;
+
+      return client.decide({
+        key: normalizeKey(key),
+        rule: {
+          limit: ruleConfig.limit,
+          window: ruleConfig.window,
+          algorithm
+        },
+        policy,
+        abuse
+      });
+
+    }
+
+  };
+
 }
 
 function normalizeKey(key) {
